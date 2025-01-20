7e5554696085b3c0715417b4207148c157d7e1ad
SavoldiFederico <federico.savoldi1111@gmail.com>
2025-01-20 10:18:39 +0100

    first

---
+++ src/main/java/com/generation/vsnbackend/VsnbackendApplication.java
@@ -0,0 +1,15 @@
+package com.generation.vsnbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsnbackendApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(VsnbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/vsnbackend/VsnbackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.generation.vsnbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsnbackendApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
