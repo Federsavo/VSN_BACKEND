ee9fc665baf3ed392a3450c89660ba8527397cb8
SavoldiFederico <federico.savoldi1111@gmail.com>
2025-01-23 12:11:34 +0100

    added all steam api endpoints

--- src/main/java/com/generation/vsnbackend/controller/SteamAPIService.java
+++ src/main/java/com/generation/vsnbackend/controller/SteamAPIService.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class SteamAPIService {
 
-    private static String key="F94F9544AEB0C802112742B29BB670DD";
+    private final static String key="F94F9544AEB0C802112742B29BB670DD";
 
     @Autowired
     private RestTemplate restTemplate;
@@ -30,7 +30,17 @@ public class SteamAPIService {
         return restTemplate.getForObject(url, String.class);
     }
 
+    public String getPublishedFiles(String steamId, String appId, int fileType)
+    {
+        String url="https://api.steampowered.com/IPublishedFileService/GetUserFiles/v1/?key="+key+"&appid="+appId+"&steamid="+steamId+"&filetype="+fileType+"&format=json";
+        return restTemplate.getForObject(url, String.class);
+    }
 
+    public String getPlayerGames(String steamId)
+    {
+        String url="http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="+key+"&steamid="+steamId+"&include_appinfo=true&format=json";
+        return restTemplate.getForObject(url, String.class);
+    }
 
 
 }
