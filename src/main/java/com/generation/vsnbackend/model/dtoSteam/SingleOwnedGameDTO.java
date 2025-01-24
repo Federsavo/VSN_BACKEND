021f58fbdd2cafae745a4b63c137ba02f249d11e
SavoldiFederico <federico.savoldi1111@gmail.com>
2025-01-24 11:25:24 +0100

    general fixes

--- src/main/java/com/generation/vsnbackend/model/dtoSteam/SingleOwnedGameDTO.java
+++ src/main/java/com/generation/vsnbackend/model/dtoSteam/SingleOwnedGameDTO.java
@@ -5,6 +5,8 @@ public class SingleOwnedGameDTO
 	private String appId;
 	private String videogameName;
 	private String iconImgUrl;
+	private String softwareHouse;
+	private int numberOfStars; //1 a 10
 
 	public String getAppId()
 	{
@@ -34,5 +36,25 @@ public class SingleOwnedGameDTO
 	public void setIconImgUrl(String iconImgUrl)
 	{
 		this.iconImgUrl = iconImgUrl;
+	}
+
+	public String getSoftwareHouse()
+	{
+		return softwareHouse;
+	}
+
+	public void setSoftwareHouse(String softwareHouse)
+	{
+		this.softwareHouse = softwareHouse;
+	}
+
+	public int getNumberOfStars()
+	{
+		return numberOfStars;
+	}
+
+	public void setNumberOfStars(int numberOfStars)
+	{
+		this.numberOfStars = numberOfStars;
 	}
 }
